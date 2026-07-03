89fa0616f2768cea44ab4aeef7a124efd7f3794f
kanikarapubhanup-tech <kanikarapubhanup@gmail.com>
2026-07-03 15:17:09 +0530

    chore: point api baseURL to production domain

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.19:5000/api',
+  baseURL: 'https://pharma-erp-pharma-backend.rrh5yv.easypanel.host/api',
   headers: {
     'Content-Type': 'application/json',
   },
